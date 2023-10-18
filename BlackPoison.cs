b8d7489f491b92b481bba125ebbf125a7687e101
Alexandra-Pharmic <129680122+Alexandra-Pharmic@users.noreply.github.com>
2023-10-18 19:48:45 +0200

    Add files via upload

---
+++ BlackPoison.cs
@@ -0,0 +1,124 @@
+﻿using LBoL.Base;
+using LBoL.ConfigData;
+using LBoL.Core.Cards;
+using LBoL.Core.Battle.BattleActions;
+using LBoL.EntityLib.EnemyUnits.Normal;
+using LBoLEntitySideloader;
+using LBoLEntitySideloader.Attributes;
+using LBoLEntitySideloader.Entities;
+using LBoLEntitySideloader.Resource;
+using MonoMod.RuntimeDetour.Platforms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LBoL.Core.Battle;
+using LBoL.Core;
+using LBoL.Core.StatusEffects;
+using JetBrains.Annotations;
+using LBoL.EntityLib.StatusEffects.Neutral.TwoColor;
+
+namespace GruulDancer
+{
+    [OverwriteVanilla]
+    public sealed class BlackPoisonDefinition : CardTemplate
+    {
+        public override IdContainer GetId()
+        {
+            return nameof(LBoL.EntityLib.Cards.Neutral.TwoColor.BlackPoison);
+        }
+
+        [DontOverwrite]
+        public override CardImages LoadCardImages()
+        {
+            return null;
+        }
+
+        public override LocalizationOption LoadLocalization()
+        {
+            var loc = new GlobalLocalization(BepinexPlugin.embeddedSource);
+            loc.LocalizationFiles.AddLocaleFile(LBoL.Core.Locale.En, "CardsEn.yaml");
+            return loc;
+        }
+
+        public override CardConfig MakeConfig()
+        {
+            var cardConfig = new CardConfig(
+              Index: BepinexPlugin.sequenceTable.Next(typeof(CardConfig)),
+              Id: "BlackPoison",
+              Order: 10,
+              AutoPerform: true,
+              Perform: new string[0][],
+              GunName: "Simple1",
+              GunNameBurst: "Simple1",
+              DebugLevel: 0,
+              Revealable: false,
+              IsPooled: true,
+              HideMesuem: false,
+              IsUpgradable: true,
+              Rarity: Rarity.Uncommon,
+              Type: CardType.Ability,
+              TargetType: TargetType.Self,
+              Colors: new List<ManaColor>() { ManaColor.Green, ManaColor.Red},
+              IsXCost: false,
+              Cost: new ManaGroup() { Red = 1, Green = 1},
+              UpgradedCost: null,
+              MoneyCost: null,
+              Damage: null,
+              UpgradedDamage: null,
+              Block: null,
+              UpgradedBlock: null,
+              Shield: null,
+              UpgradedShield: null,
+              Value1: 1,
+              UpgradedValue1: 2,
+              Value2: null,
+              UpgradedValue2: null,
+              Mana: null,
+              UpgradedMana: null,
+              Scry: null,
+              UpgradedScry: null,
+              ToolPlayableTimes: null,
+
+              Loyalty: null,
+                UpgradedLoyalty: null,
+                PassiveCost: null,
+                UpgradedPassiveCost: null,
+                ActiveCost: null,
+                UpgradedActiveCost: null,
+                UltimateCost: null,
+                UpgradedUltimateCost: null,
+
+              Keywords: Keyword.None,
+              UpgradedKeywords: Keyword.None,
+              EmptyDescription: false,
+              RelativeKeyword: Keyword.None,
+              UpgradedRelativeKeyword: Keyword.None,
+
+              RelativeEffects: new List<string>() { },
+              UpgradedRelativeEffects: new List<string>() { },
+              RelativeCards: new List<string>() { },
+              UpgradedRelativeCards: new List<string>() { },
+              Owner: null,
+              Unfinished: false,
+              Illustrator: null,
+              SubIllustrator: new List<string>() { }
+           );
+
+            return cardConfig;
+        }
+
+        [EntityLogic(typeof(BlackPoisonDefinition))]
+        [UsedImplicitly]
+        public sealed class BlackPoison : Card
+        {
+            protected override IEnumerable<BattleAction> Actions(UnitSelector selector, ManaGroup consumingMana, Interaction precondition)
+            {
+                yield return base.BuffAction<BlackPoisonSe>(base.Value1, 0, 0, 0, 0.2f);
+                yield break;
+            }
+
+        }
+    }
+
+
+}
---
+++ PInfo.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace GruulDancer
+{
+    public static class PInfo
+    {
+        // each loaded plugin needs to have a unique GUID. usually author+generalCategory+Name is good enough
+        public const string GUID = "alexi.lbol.gruuldancer";
+        public const string Name = "GruulDancer";
+        public const string version = "1.0.0";
+        public static readonly Harmony harmony = new Harmony(GUID);
+
+    }
+}
